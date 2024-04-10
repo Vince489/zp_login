b603169b6b38971d1af941eaff30347f2a1f4b34
Vince489 <vincentcornelius489@gmail.com>
2024-04-10 06:56:47 -0400

    change

---
+++ log2.js
@@ -0,0 +1,32 @@
+router.post('/login', async (req, res) => {
+  try {
+    // Extract user data from request body
+    const { email, password } = req.body;
+
+    // Check if email and password are provided
+    if (!email || !password) {
+      return res.status(400).json({ message: 'email and password are required' });
+    }
+
+    // Authenticate user
+    const { user, token } = await authenticateUser({ email, password });
+
+    // Set the JWT token as a secure cookie
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: true, // Set to true if your app uses HTTPS
+      sameSite: 'strict' // Set the sameSite attribute to mitigate CSRF attacks
+    });
+
+    // Authentication successful
+    res.status(200).json({ 
+      message: 'Login successful',
+      user: user,
+      token: token, 
+    });
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
---
+++ src/domains/user/controller.js
@@ -0,0 +1,73 @@
+const User = require("../user/model");
+const { hashData } = require("./../../utils/hashData");
+const createToken = require("./../../utils/createToken");
+const { verifyHashedData } = require("./../../utils/hashData");
+
+
+const createNewUser = async (data) => {
+  try {
+    const { userName, codeName, email, password } = data;
+
+    // Checking if user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw Error("User with the provided email already exists");
+    }
+
+    // hash password
+    const hashedPassword = await hashData(password);
+    const newUser = new User({
+      userName,
+      codeName,
+      email,
+      password: hashedPassword,
+    });
+    // save user
+    const createdUser = await newUser.save();
+    return createdUser;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const authenticateUser = async (data) => {
+  try {
+    const { email, password } = data;
+
+    // Find user by username
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error("Invalid username or password");
+    }
+
+    if (!user.verified) {
+      throw Error("Email hasn't been verified yet. Check your inbox.");
+    }
+
+    // Verify password
+    const hashedPassword = user.password;
+    const passwordMatch = await verifyHashedData(password, hashedPassword);
+
+    if (!passwordMatch) {
+      throw new Error("Invalid username or password entered");
+    }
+
+    // Update isAuthenticated field to true
+    user.isAuthenticated = true;
+
+    // Generate JWT token
+    const tokenData = { userId: user._id, email, isAuthenticated: user.isAuthenticated};
+    const token = await createToken(tokenData);
+
+    // Assign token to user
+    user.token = token;
+
+    return { user, token };
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { createNewUser, authenticateUser };
